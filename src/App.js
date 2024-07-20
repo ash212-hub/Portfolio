43c1de99e0f080fcb1b128fc31f0180aa217aa19
ashif shaik <ashifshaik000@gmail.com>
2024-07-20 12:39:52 +0530

    Signed-off-by: ashif shaik <ashifshaik000@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState,useEffect } from 'react'; 
+import "./App.css"
+import Logo from './Logo';
+import Home from './Home';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+ 
+    //  console.log(isLoading);
+  useEffect(() => {
+  
+     
+    setTimeout(() => {
+      
+        setIsLoading(false);
+        
+    }, 10000);
+}, []);
+  
+  return (
+    <div className='App w-100 h-100   '>
+       {isLoading?(<Logo />):(<Home/>)} 
+    </div>
+  )
+}
+
+export default App
